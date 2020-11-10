65c4789c7df60ea39ab702b3c036f41e91fd2173
Rinat <lich666black@gmail.com>
2020-11-10 16:53:11 +0500

    fxied total count

--- src/zil/total-supply.js
+++ src/zil/total-supply.js
@@ -7,7 +7,7 @@ const contract = process.env.DRAGONZIL;
 const zilliqa = new Zilliqa(provider);
 
 module.exports = async function() {
-  const field = 'total_supply';
+  const field = 'count_supply';
 
   const { result } = await zilliqa
     .blockchain
